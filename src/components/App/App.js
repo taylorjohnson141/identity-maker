7fc659b32910d50797296dd097a328fa44fbc423
Tim Keresey <tim.keresey@gmail.com>
2020-10-29 14:02:15 -0600

    create IDForm component element

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Header from '../Header/Header.js';
+import IDForm from '../IDForm/IDForm.js';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
---
+++ src/components/IDForm/IDForm.js
@@ -0,0 +1,21 @@
+import './IDForm.css';
+
+
+const IDForm = () => {
+  return (
+    <form>
+      <label for='first-name'>first name</label>
+      <input name='first-name' placeholder='first name'/>
+      <label for='first-name'>first name</label>
+      <input name='last-name' placeholder='last name'/>
+      <label for='country'>target country</label>
+      <input list='countries' name='country' id='countries'/>
+      <datalist id='countries'>
+
+      </datalist>
+      <input type='submit'/>
+    </form>
+  );
+}
+
+export default IDForm;
