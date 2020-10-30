917534f1bb1727bcd3a54a531132322cef801bcd
Eric Berglund <ericberglund117@gmail.com>
2020-10-29 18:12:33 -0600

    Comment out actions in order to render page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-export const addID = (identity) => ({
-  type: 'ADD_ID',
-  identity
-});
-
-export const findCountry = (country) => ({
-  type: 'FIND_COUNTRY',
-  country
-});
+// export const addID = (identity) => ({
+//   type: 'ADD_ID',
+//   identity
+// });
+//
+// export const findCountry = (country) => ({
+//   type: 'FIND_COUNTRY',
+//   country
+// });
