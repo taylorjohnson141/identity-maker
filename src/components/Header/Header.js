b04b33007b2559517beb0efc41ab29ac6ca93dda
Eric Berglund <ericberglund117@gmail.com>
2020-10-31 13:31:19 -0600

    Add styling to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import './Header.css';
+import SpyIcon from '../../Images/spyIcon.png';
 
 const Header = () => {
   return (
     <section className='header-section'>
+      <section className='background'>
+        <img alt='spy-icon' src={SpyIcon} />
       <h1>Welcome, Agent</h1>
+      </section>
     </section>
   );
 }
