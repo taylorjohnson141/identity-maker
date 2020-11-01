1227eb335a2c8e9c3ac79aec2049eafacc316380
Eric Berglund <ericberglund117@gmail.com>
2020-11-01 11:22:50 -0700

    Create fetchCountries api call

--- src/thunks/fetchCountries.js
+++ src/thunks/fetchCountries.js
@@ -1,4 +1,18 @@
+import { isLoading, hasErrored, setCountries } from '../actions/index';
 
 export const fetchCountries = (url) => {
-  return async
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url)
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      const data = await response.json()
+      dispatch(isLoading(false))
+      dispatch(setCountries(data))
+    } catch (error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
 }
