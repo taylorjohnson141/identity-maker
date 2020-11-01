869d5d46490393e76cd2f8741673731b74165fce
Eric Berglund <ericberglund117@gmail.com>
2020-11-01 11:02:06 -0700

    Update input fields in form

--- src/components/IDForm/IDForm.js
+++ src/components/IDForm/IDForm.js
@@ -18,28 +18,99 @@ const IDForm = () => {
   const countries = useSelector(state => state.countries)
 
 
-  
+
   // if(!countries){
   //   return <h1>Loading</h1>
   // }
   return (
-    
+
     <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="" className="">
+      Enter Three First Names
+      </label>
       <input
-        name="first-name"
+        name="first-name1"
         ref={register({
           required: "Required",
         })}
       />
 
       <input
+        name="first-name2"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <input
+        name="first-name3"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <label htmlFor="" className="">
+      Enter A Last Name
+      </label>
+      <input
         name="last-name"
         ref={register({
           required: "Required",
         })}
       />
+
+      <label htmlFor="" className="">
+      Enter A Company Name
+      </label>
+      <input
+        name="company-name"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <label htmlFor="" className="">
+      Enter An Occupation
+      </label>
+      <input
+        name="occupation-name"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <label htmlFor="" className="">
+      Enter A Restaurant Name
+      </label>
+      <input
+        name="restaurant-name"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <label htmlFor="" className="">
+      Enter A Domestic Animal(singular)
+      </label>
+      <input
+        name="singular-animal-name"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
+      <label htmlFor="" className="">
+      Enter A Domestic Animal(plural)
+      </label>
+      <input
+        name="plural-animal-name"
+        ref={register({
+          required: "Required",
+        })}
+      />
+
       <input name="country "type="text" list="data" />
-      <datalist 
+      <datalist
        ref={register({
         required: "Required",
       })}
@@ -53,4 +124,3 @@ const IDForm = () => {
 }
 
 export default IDForm;
-
