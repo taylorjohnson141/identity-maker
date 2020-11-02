20bd55a61664003800f6a59d9669b34559ab5552
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-01 19:02:13 -0700

    Add country flag

--- src/components/MissionArea/MissionArea.js
+++ src/components/MissionArea/MissionArea.js
@@ -5,13 +5,14 @@ const MissionArea = (props) => {
   console.log(currentCountry)
   return (
     <article>
-        <h1>`{currentCountry.nativeName}</h1>
+      <img src={currentCountry.flag} alt = 'country flag'/>
+      <h1>{currentCountry.nativeName}</h1>
       {`You will travel to ${currentCountry.subregion} 
       and imbed yourself in ${currentCountry.name},
        a nation of ${currentCountry.population} people. 
        Your mission’s target location is ${currentCountry.capital}. 
        The currency you will require is the ${currentCountry.currencies[0].name} and an amount of ${currentCountry.currencies[0].symbol}
-       10,000 will be transferred to your account. You will need to learn ${currentCountry.languages[0].name} 
+       10,000 will be transferred to your account. You will need to learn ${currentCountry.languages[0].name}
        flawlessly in order to blend in with the locals. You will be tasked with infiltrating high level diplomatic offices and 
        reporting back as much info on there dealings with other nations of the ${currentCountry.regionalBlocs[0].name} || ${currentCountry.region}. 
        If your cover is blown… good luck.`}
