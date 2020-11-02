7548298d20cfe63584988c1073c3db607368ecd1
Tim Keresey <tim.keresey@gmail.com>
2020-11-02 15:55:59 -0700

    add unit test for id area render

--- src/components/IDArea/IDArea.js
+++ src/components/IDArea/IDArea.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './IDArea.css';
 import Identity from '../Identity/Identity.js';
-import {useSelector} from 'react-redux'
+import {useSelector} from 'react-redux';
 
 
 const IDArea = () => {
   let identities = useSelector(state => {
     return state.identities
   })
-  let createdIdentities = identities.map((identity,index )=>{
+  let createdIdentities = identities.map((identity, index)=>{
     return <Identity key ={index} identities = {identity} />
   })
-  if(createdIdentities.length ===0 ){
+  if(createdIdentities.length === 0){
     return <h1>Make an ID</h1>
   }
   return (
---
+++ src/components/IDArea/IDArea.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import IDArea from './IDArea.js';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../reducers/index.js';
+
+
+describe('IDArea', () => {
+  it('should render the identity card area with a message asking the user to make an ID if they have not', () => {
+    const store = createStore(rootReducer);
+
+    render(<Provider store={store}><IDArea /></Provider>);
+
+    expect(screen.getByText('Make an ID')).toBeInTheDocument();
+  });
+
+})
