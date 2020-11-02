ca5a7d51dbef72b370e7232cfa8b521cb5b5725a
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-02 15:30:26 -0700

    Add Mission area test bones

---
+++ src/components/MissionArea/MissionArea.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import MissionArea from './MissionArea.js';
+import { render, screen } from '@testing-library/react';
+
+
+describe('MissionArea', () => {
+})
