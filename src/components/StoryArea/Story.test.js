5fcaa4040cca7d001b6b712856693834357dd54c
Taylor Johnson <taylorjohnson141@gmail.com>
2020-11-02 15:17:12 -0700

    Add Initial storyarea test

---
+++ src/components/StoryArea/Story.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import IDForm from './IDForm.js';
+import { render, screen } from '@testing-library/react';
+
+
+describe('StoryArea', () => {
+  it('should render the Story Area correctly', () => {
+    
+  })
+})
