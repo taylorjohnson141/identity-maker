f2f14a612e4b5d1ed14d20cc705c3147d326f7e6
Eric Berglund <ericberglund117@gmail.com>
2020-11-02 17:18:52 -0700

    Update IDPage test

--- src/components/IDPage/IDPage.test.js
+++ src/components/IDPage/IDPage.test.js
@@ -2,30 +2,28 @@ import React from 'react';
 import IDPage from './IDPage';
 import StoryArea from '../StoryArea/StoryArea'
 import { screen, render } from '@testing-library/react';
-import Enzyme from 'enzyme';
-import { mount, shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import rootReducer from '../../reducers/index.js'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+describe('IDPage', () => {
+  it('should render a storyArea', () => {
+    const store = createStore(rootReducer)
+    const mockIdentityNumber = '2'
 
-// describe('IDPage', () => {
-//   // const mockStore = createStore(reducer, {identity: []});
-//   // const getWrapper = () => mount(
-//   //   <Provider store={mockStore}>
-//   //     <IDPage />
-//   //   </Provider>
-//   // )
-//
-//   it('should render a storyArea', () => {
-//     render(<IDPage />)
-//     const currentIdentity = 'Michael Scarn'
-//     const currentCountry = 'Madagascar'
-//     const wrapper = shallow(
-//       <StoryArea currentIdentity={currentIdentity} currentCountry={currentCountry} />
-//     )
-//
-//     expect(wrapper.prop('currentIdentity')).toEqual(currentIdentity);
-//   });
-// });
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <IDPage id={mockIdentityNumber}/>
+        </BrowserRouter>
+      </Provider>
+    )
+
+    //IDPage does not render anything directly.
+    // Takes props(id) and finds the currentIdentity and currentCountry
+    // uses CI and CC to render StoryArea and MissionArea
+
+  });
+});
