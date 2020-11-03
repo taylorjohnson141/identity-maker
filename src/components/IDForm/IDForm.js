54a5091342b045da33eeb68ff4d1a3e97863816b
Tim Keresey <tim.keresey@gmail.com>
2020-11-03 09:13:11 -0700

    reorganize form inputs into divs

--- src/components/IDForm/IDForm.js
+++ src/components/IDForm/IDForm.js
@@ -32,183 +32,211 @@ const IDForm = () => {
   return (
 
     <form className='id-form' name='identity' onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="" className="">
-      Enter Three First Names
-      </label>
-      <input
-        name="first-name1"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter Three First Names
+        </label>
+        <input
+          name="first-name1"
+          ref={register({
+            required: "Required",
+          })}
+        />
+        <input
+          name="first-name2"
+          ref={register({
+            required: "Required",
+          })}
+        />
+        <input
+          name="first-name3"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <input
-        name="first-name2"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Last Name
+        </label>
+        <input
+          name="last-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <input
-        name="first-name3"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Company Name
+        </label>
+        <input
+          name="company-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Last Name
-      </label>
-      <input
-        name="last-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter An Occupation
+        </label>
+        <input
+          name="occupation-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Company Name
-      </label>
-      <input
-        name="company-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Restaurant Name
+        </label>
+        <input
+          name="restaurant-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter An Occupation
-      </label>
-      <input
-        name="occupation-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Domestic Animal(singular)
+        </label>
+        <input
+          name="singular-animal-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Restaurant Name
-      </label>
-      <input
-        name="restaurant-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Domestic Animal(plural)
+        </label>
+        <input
+          name="plural-animal-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Domestic Animal(singular)
-      </label>
-      <input
-        name="singular-animal-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter Two Numbers Between 1 And 10
+        </label>
+        <input
+          name="numbers1"
+          type='number'
+          min='1'
+          max='10'
+          ref={register({
+            required: "Required",
+          })}
+        />
+        <input
+          name="numbers2"
+          type='number'
+          min='1'
+          max='10'
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Domestic Animal(plural)
-      </label>
-      <input
-        name="plural-animal-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Sport
+        </label>
+        <input
+          name="sport-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter Two Numbers Between 1 And 10
-      </label>
-      <input
-        name="numbers1"
-        type='number'
-        min='1'
-        max='10'
-        ref={register({
-          required: "Required",
-        })}
-      />
-      <input
-        name="numbers2"
-        type='number'
-        min='1'
-        max='10'
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Game
+        </label>
+        <input
+          name="game-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Sport
-      </label>
-      <input
-        name="sport-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter Two Genres
+        </label>
+        <input
+          name="genre1"
+          ref={register({
+            required: "Required",
+          })}
+        />
+        <input
+          name="genre2"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Game
-      </label>
-      <input
-        name="game-name"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter A Color
+        </label>
+        <input
+          name="color-name"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter Two Genres
-      </label>
-      <input
-        name="genre1"
-        ref={register({
-          required: "Required",
-        })}
-      />
-      <input
-        name="genre2"
-        ref={register({
-          required: "Required",
-        })}
-      />
+      <div className='form-part'>
+        <label htmlFor="" className="">
+        Enter An Animal You'd See In A Zoo
+        </label>
+        <input
+          name="zoo-animal"
+          ref={register({
+            required: "Required",
+          })}
+        />
+      </div>
 
-      <label htmlFor="" className="">
-      Enter A Color
-      </label>
-      <input
-        name="color-name"
-        ref={register({
+      <div className='form-part'>
+        <label htmlFor="" className="country-selection">
+        Enter A Name Of A Country
+        </label>
+        <input name="country" list="data"
+          ref={register({
           required: "Required",
-        })}
-      />
+       })}/>
+        <datalist
+         id="data"
+         name="country"
+        >
+          {countryList}
+        </datalist>
+      </div>
 
-      <label htmlFor="" className="">
-      Enter An Animal You'd See In A Zoo
-      </label>
-      <input
-        name="zoo-animal"
-        ref={register({
-          required: "Required",
-        })}
-      />
-
-      <label htmlFor="" className="country-selection">
-      Enter A Name Of A Country
-      </label>
-      <input name="country" list="data"
-        ref={register({
-        required: "Required",
-     })}/>
-      <datalist
-       id="data"
-       name="country"
-      >
-        {countryList}
-      </datalist>
-
-      <button type="submit">Submit</button>
+      <div className='form-part'>
+        <button type="submit">Submit</button>
+      </div>
     </form>
   )
 }
