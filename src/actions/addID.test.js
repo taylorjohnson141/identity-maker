67f646ad70b7621ac629c8ebcd77c8d30c40341a
Eric Berglund <ericberglund117@gmail.com>
2020-11-03 14:25:12 -0700

    Pass addID action test

---
+++ src/actions/addID.test.js
@@ -0,0 +1,35 @@
+import * as actions from '../actions'
+
+describe('actions', () => {
+  it('should have a type of ADD_ID', () => {
+    const identity = {
+      color: 'red',
+      company: 'Bic',
+      country: 'Belguim',
+      firstName1: 'John',
+      firstName2: 'Wayne',
+      firstName3: 'Pilgrim',
+      game: 'backgammon',
+      genre1: 'mystery',
+      genre2: 'comedy',
+      lasName: 'Funkstein',
+      numbers1: 2,
+      numbers2: 5,
+      occupation: 'inspector',
+      pluralAnimal: 'zebras',
+      singularAnimal: 'moose',
+      sport: 'rugby',
+      zooAnimal: 'lemur'
+    }
+
+    const expectedAction = {
+      type: 'ADD_ID',
+      id: Date.now(),
+      identity: identity
+    }
+
+    const result = actions.addID(identity);
+
+    expect(result).toEqual(expectedAction);
+  })
+})
